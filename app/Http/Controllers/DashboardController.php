d88b3a9111e18fd0053c6bb7e8da4f79dec4d308
Ashwin <ashwinnyamainashe@gmail.com>
2022-09-11 12:23:45 +0200

    Corrected an error with database final2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,17 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class DashboardController extends Controller
-{
-    public function index(){
-        if(Auth::user()->hasRole('user')){
-            return view('userdash');
-        }
-    elseif(Auth::user()->hasRole('admin')){
-        return view('admindash');
-    }
-    }
+class DashboardController extends Controller{
+// {
+//     public function index(){
+//         if(Auth::user()->hasRole('user')){
+//             return view('userdash');
+//         }
+//     elseif(Auth::user()->hasRole('admin')){
+//         return view('admindash');
+//     }
+//     }
 
+        public function index(){
+            
+                return view('userdash');
+            }
     public function myprofile()
     {
         return view('myprofile');
