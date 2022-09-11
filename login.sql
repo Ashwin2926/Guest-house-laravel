6c00bb318e7d5c5086717aac456c2448bf8aff2a
Ashwin <ashwinnyamainashe@gmail.com>
2022-09-11 11:44:51 +0200

    Corrected an error with database final2

--- login.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'ashwin', 'ashwinnyamainashe@gmail.com', NULL, 'munashe123', NULL, '2022-08-25 13:08:23', '2022-08-28 03:13:14');
