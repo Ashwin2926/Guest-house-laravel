08d461a14e57b0fdf2f748976684457a6ab25346
Ashwin <ashwinnyamainashe@gmail.com>
2022-09-11 03:39:59 +0200

    Corrected an error with database2

---
+++ login.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'ashwin', 'ashwinnyamainashe@gmail.com', NULL, 'munashe123', NULL, '2022-08-25 13:08:23', '2022-08-28 03:13:14');
