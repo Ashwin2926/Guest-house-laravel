cec249e150874d4234cc3216260603c8fa969bd9
Ashwin <ashwinnyamainashe@gmail.com>
2022-09-12 10:54:40 +0200

    Dashboard error

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -35,9 +35,12 @@
                                      <td class="px-4 py-2 border">
                                         @method('delete')
                                         @csrf
-                                            <a href="{{ route('shift', $row->id) }}" class="inline-flex items-center px-4 py-2 mx-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25">
+                                            {{-- <a href="{{ route('shift', $row->id) }}" class="inline-flex items-center px-4 py-2 mx-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25">
                                                accept
-                                            </a>
+                                            </a> --}}
+                                            <a href="#" class="inline-flex items-center px-4 py-2 mx-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25">
+                                                accept
+                                             </a>
                                             <br>
                                             <br>
                                            
